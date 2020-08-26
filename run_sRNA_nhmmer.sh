8a3dee3603de97e70e97eef3b20bb5baeb907958
Tom Nicholson <nicholson.tom.n@gmail.com>
2020-08-26 17:03:02 +1200

    updates to run on different genome/contig names. Also changed to allow for different file types

--- run_sRNA_nhmmer.sh
+++ run_sRNA_nhmmer.sh
@@ -5,20 +5,37 @@ usage(){
 Usage:
  run_sRNA_nhmmer.sh [opts] [input]
 
+Required:	       
+	-d	<database> The nucleotide database to be searched against as the nhmmer target
+	-f	<folder> The folder containing the query file for nhmmer
+	-e	<extension> The file extension of the query file in <folder>
+
 Options:
 	-h	Display this help
-
-Input	       
-	-r	Folder location
-
+	-c	Check if the sequences are >50 nt (only works on fasta files)
 "
 }
 
-while getopts "d:h" arg; do
+check_seq_lengths=""
+database=""
+folder=""
+extension=""
+
+missing=""
+exitTrue="F"
+evalue="1e-5"
+while getopts "d:f:E:e:ch" arg; do
 case $arg in
 	d) 
 	database=${OPTARG};;
-
+	f)
+	folder=${OPTARG};;
+	E)
+	evalue=${OPTARG};;
+	e)
+	extension=${OPTARG};;	
+	c)
+	check_seq_lengths="T";;
     h)
 		usage
 		exit
@@ -28,45 +45,96 @@ case $arg in
     esac
 done
 
-echo "checking for short seqs"
-mkdir -p single_seqs/short_seqs
-let "fileNum = 0"
-for file in single_seqs/*.fna
-
-do
-
-seqlength=`grep -v ^">" $file | wc -c`
-if (( $seqlength < 50 )); then
-mv $file  single_seqs/short_seqs/
+if [[ $database == "" ]]; then
+	missing="$missing -d <database> "
+	exitTrue="T"
 fi
 
-done
+
+if [[ $folder == "" ]]; then
+	missing="$missing -f <folder> "
+	exitTrue="T"	
+fi
+
+if [[ $extension == "" ]]; then
+	missing="$missing -e <extension> "
+	exitTrue="T"
+fi
+
+if [[ $exitTrue == "T" ]];then
+	echo "$missing not found. Use -h for more help."
+	exit
+fi
+
+
+if [[ $extension == "stk" ]]; then
+	check_seq_lengths=""
+fi
+
+echo "extension value: $extension"
+
+if [[ $check_seq_lengths == "T" ]]; then
+
+	echo "checking for short seqs"
+	mkdir -p $folder/short_seqs
+	
+	let "fileNum = 0"
+	for file in $folder/*.$extension
+	do
+	
+	seqlength=`grep -v ^">" $file | wc -c`
+	if (( $seqlength < 50 )); then
+	mv $file  single_seqs/short_seqs/
+	fi
+	
+	done
+
+fi
 
 echo "making directories in `pwd`"
 mkdir -p alignments
 mkdir -p hmm
 mkdir -p output
 
-
 echo "running nhmmer"
 let "fileNum = 0"
-for file in single_seqs/*.fna
+for file in $folder/*.$extension
 
 do
 
 
-outname=`basename $file .fna`
 
-nhmmer -E 1e-5 --tblout output/$outname.tbl -A alignments/tmp.stk --tformat FASTA --qformat FASTA $file $database > output/$outname.res
+
+echo $file
+outname=`basename $file`
+
+
+if [ -f "output/$outname.res" ]; then
+	echo "Already exists: $file"
+	continue
+fi
+
+
+
+
+nhmmer -E $evalue --tblout output/$outname.tbl -A alignments/tmp.stk --tformat FASTA  $file $database > output/$outname.res
+
+lines=`wc -l < alignments/tmp.stk`
+
+
+if (( $lines > 0 )); then
 
 esl-alimanip   --detrunc  60  alignments/tmp.stk > alignments/$outname.stk
 
-hmmbuild hmm/$outname.hmm alignments/$outname.stk > output/hmm.out
+hmmbuild hmm/$outname.hmm alignments/$outname.stk
 
+else
 
+echo "No hits found for $outname"
+
+fi
 
 done
-
 
 
 
--- srnaAlignAndFold.sh
+++ srnaAlignAndFold.sh
@@ -49,15 +49,59 @@ let "fileNum = 0"
 for file in alignments/*.stk
 
 do
-echo "Running on: $file"
 
 outname=`basename $file`
 
+#echo "${FOLDER}alifold/$outname.alifold"
 
+if [ -f "$FOLDER/alifold/$outname.alifold" ]; then
+	echo "Already exists: $file"
+	continue
+fi
+
+
+
+
+echo "Running on: $file"
+
+
+start=`grep "GCA" $file | head -n 1 | cut -d " " -f2 | cut -d "/" -f2 | cut -d "-" -f1`
+end=`grep "GCA" $file | head -n 1 | cut -d " " -f2 | cut -d "/" -f2 | cut -d "-" -f2`
+
+if [[ $start == "" ]]; then
+	start=`grep "NC_" $file | head -n 1 | cut -d " " -f2 | cut -d "/" -f2 | cut -d "-" -f1`
+	end=`grep "NC_" $file | head -n 1 | cut -d " " -f2 | cut -d "/" -f2 | cut -d "-" -f2`
+
+fi
+
+if [[ $start == "" ]]; then
+	start=`grep "NZ_" $file | head -n 1 | cut -d " " -f2 | cut -d "/" -f2 | cut -d "-" -f1`
+	end=`grep "NZ_" $file | head -n 1 | cut -d " " -f2 | cut -d "/" -f2 | cut -d "-" -f2`
+
+fi
+
+if [[ $start == "" ]]; then
+	head $file
+fi
+
+
+
+
+length=`expr $end - $start`
+
+if (( $length < 0 )); then
+	length=$(( -1 * $length ))
+fi
+
+if (( $length < 750 )); then
 esl-reformat  clustal $file  | RNAalifold --aln-stk=${file} >> ./RNAalifold/$outname.rnaalifold
 cat alirna.ps > ./alifold/post_script/$outname.ps      
 esl-reformat  clustal $file  | alifoldz.pl > ./alifold/$outname.alifold         
 
 R-scape --r2rall --outdir ./rscape_out/ $file
+
+else
+	echo "Skipping: $file"
+fi
 
 done
